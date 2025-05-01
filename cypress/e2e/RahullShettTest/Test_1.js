ae118a1a494eb049ee6db1f334d45f91e28018f1
Ahmet Kurcak <ahmetk@student.uia.no>
2025-05-01 11:33:17 +0200

    added handling visible

--- cypress/e2e/RahullShettTest/Test_1.js
+++ cypress/e2e/RahullShettTest/Test_1.js
@@ -1,5 +1,8 @@
-describe("My First Tset Suite", () => {
-  it("My FistTest case", () => {
+describe("My First Test Suite", () => {
+  it("My FirstTest case", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product:visible").should("have.length", 4);
   });
 });

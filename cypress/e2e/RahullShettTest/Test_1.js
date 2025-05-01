80de53d977d440ece4b8f823608ec8ac312e1a4a
Ahmet Kurcak <ahmetk@student.uia.no>
2025-05-01 12:13:15 +0200

    Get and find commands with Cypress

--- cypress/e2e/RahullShettTest/Test_1.js
+++ cypress/e2e/RahullShettTest/Test_1.js
@@ -3,6 +3,12 @@ describe("My First Test Suite", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
+    // selenium get hit url in browser, cypress get acts like findElement of selenium
+    cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
+    // Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(":nth-child(3) > .product-action > button").click();
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
   });
 });

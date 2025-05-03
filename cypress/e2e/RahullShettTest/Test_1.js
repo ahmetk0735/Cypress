61a40008e245bfc11d6c3246ed0bf753821584bb
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-03 23:40:56 +0200

    test: implement Cypress test with search, product filtering, and brand logging

--- cypress/e2e/RahullShettTest/Test_1.js
+++ cypress/e2e/RahullShettTest/Test_1.js
@@ -10,5 +10,21 @@ describe("My First Test Suite", () => {
     cy.get(".products").find(".product").should("have.length", 4);
     cy.get(":nth-child(3) > .product-action > button").click();
     cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          $el.find("button").click();
+        }
+      });
+
+    cy.get(".brand").then(function (logoelement) {
+      cy.log(logoelement.text());
+    });
+    const logo = cy.get(".brand");
+    cy.log(cy.get(".brand").text());
+    //cy.log(logo.text())
   });
 });

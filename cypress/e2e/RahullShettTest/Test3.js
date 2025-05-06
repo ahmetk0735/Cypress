adf5c005b429390f18f6c1d2005ca9fa2774c6e3
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-06 13:38:26 +0200

    Added Static Dropdown

--- cypress/e2e/RahullShettTest/Test3.js
+++ cypress/e2e/RahullShettTest/Test3.js
@@ -1,11 +1,15 @@
 describe("Automate check boxes", () => {
   it("My firstTest case", () => {
+    // Check boxes
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("#checkBoxOption1")
       .check()
       .should("be.checked")
       .and("have.value", "option1");
     cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
-    cy.get('input[type="checkbox"]').check(["option1", "option2", "option3"]);
+    cy.get('input[type="checkbox"]').check(["option2", "option3"]);
+
+    // Static Dropdown
+    cy.get("select").select("option2").should("have.value", "option2");
   });
 });

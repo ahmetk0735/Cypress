fc2d1b49168b4e67df596794bc2fb259801bcbd5
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-06 12:23:24 +0200

    Checkboxes testing

---
+++ cypress/e2e/RahullShettTest/Test3.js
@@ -0,0 +1,11 @@
+describe("Automate check boxes", () => {
+  it("My firstTest case", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("#checkBoxOption1")
+      .check()
+      .should("be.checked")
+      .and("have.value", "option1");
+    cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
+    cy.get('input[type="checkbox"]').check(["option1", "option2", "option3"]);
+  });
+});

52e6ae8a94bcc4ccd5cd04d1f5dfd0a4e38f4863
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-07 11:08:17 +0200

    Dynamic dropdowns testing

--- cypress/e2e/RahullShettTest/Test3.js
+++ cypress/e2e/RahullShettTest/Test3.js
@@ -11,5 +11,14 @@ describe("Automate check boxes", () => {
 
     // Static Dropdown
     cy.get("select").select("option2").should("have.value", "option2");
+
+    // Dynamic dropdowns
+    cy.get("#autocomplete").type("ind");
+    cy.get(".ui-menu-item div").each(($e1, index, $list) => {
+      if ($e1.text() === "India") {
+        $e1.click();
+      }
+    });
+    cy.get("#autocomplete").should("have.value", "India");
   });
 });

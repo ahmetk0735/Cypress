a7827bb69332c930ea83413d779b2b4ad499dce0
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-19 22:44:42 +0200

    Handling iframe

---
+++ cypress/e2e/RahullShettTest/demo.js
@@ -0,0 +1,10 @@
+import "cypress-iframe";
+describe("Frames Test", () => {
+  it("Demo example", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.wait(2000);
+    cy.frameLoaded("#courses-iframe");
+    cy.iframe().find("a[href*='mentorship']").eq(0).click();
+    cy.iframe().find("h1[class*='pricing-title']").should("have.length", 2);
+  });
+});

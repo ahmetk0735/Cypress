473934500b7b7316e768c93e97b0c3a922003c0c
Ahmet Kurcak <kurcakahmet07@gmail.com>
2025-05-21 12:39:55 +0200

    Calendar

---
+++ cypress/e2e/RahullShettTest/calendar.js
@@ -0,0 +1,12 @@
+describe("Calendar test", () => {
+  it("Verify date selection", () => {
+    const monthNumber = "6";
+    const date = "15";
+    const year = "2027";
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+    cy.get(".react-date-picker__inputGroup").click();
+    cy.get(".react-calendar__navigation__label").click();
+    cy.get(".react-calendar__navigation__label").click();
+    cy.contains("button", year).click();
+  });
+});
